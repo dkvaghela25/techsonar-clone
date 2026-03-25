596548a6de5dd3026467bc75f6418ba28a309155
Divyang Vaghela <dkvaghela724@gmail.com>
2026-03-25 20:19:59 +0530

    Added animation in hamburger

--- src/components/ui/Header.jsx
+++ src/components/ui/Header.jsx
@@ -12,11 +12,16 @@ const Header = () => {
 
 
     return (
-        <div className="w-[80%] max-sm:w-full m-auto max-sm:m-0 p-5 flex items-center justify-between font-medium">
+        <div className="w-[90%] max-sm:w-full m-auto max-sm:m-0 p-5 flex items-center justify-between font-medium">
             <img className="w-45 max-sm:w-30" src="./images/logo.svg" alt="" />
 
-            <div className={`max-sm:absolute flex gap-40 max-sm:p-3 max-sm:flex-col max-sm:items-center max-sm:gap-2 max-sm:top-15 max-sm:right-0 max-sm:w-full max-sm:bg-secondary-bg max-sm:rounded-sm max-sm:text-sm ${!isVisible ? "max-sm:hidden" : ""} `}>
-                <div className="flex max-sm:flex-col max-sm:text-center max-sm:px-0 max-sm:gap-2 gap-10 bg-secondary-bg text-secondary-text px-6 py-3 max-sm:py-0 rounded-sm">
+            <div
+                className={`max-sm:absolute flex gap-30 max-sm:p-3 max-sm:flex-col max-sm:items-center max-sm:gap-2 max-sm:top-15 max-sm:right-0 max-sm:w-full max-sm:bg-secondary-bg max-sm:rounded-sm max-sm:text-sm transition-all duration-300 ease-in-out  
+                    ${isVisible
+                        ? "max-sm:opacity-100 "
+                        : "max-sm:opacity-0 max-sm:-translate-y-5"
+                    }`}
+            >                <div className="flex max-sm:flex-col max-sm:text-center max-sm:px-0 max-sm:gap-2 gap-10 bg-secondary-bg text-secondary-text px-6 py-3 max-sm:py-0 rounded-sm">
                     <Link to="/">Why TechSonar</Link>
                     <Link to="/">Features</Link>
                     <Link to="/">Faq</Link>
